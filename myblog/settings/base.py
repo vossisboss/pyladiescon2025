9dacfbdfa4e7fcc7485ef52823f05bc80981dee7
Meagen Voss <45881480+vossisboss@users.noreply.github.com>
2025-11-06 19:30:30 -0500

    Removed custom models for new tutorial.

--- myblog/settings/base.py
+++ myblog/settings/base.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     "home",
     "blog",
     "search",
-    "custom_media",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.embeds",
@@ -174,5 +173,3 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = "http://example.com"
-
-WAGTAILIMAGES_IMAGE_MODEL = "custom_media.CustomImage"
